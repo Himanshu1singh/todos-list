914353952a1e1019108a1ce3500e81b10bd923f4
Himanshu Singh <himanshucool1001@gmail.com>
2021-07-22 15:50:53 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about conponent
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id quasi expedita culpa quae magni beatae. Quis sint cupiditate praesentium perferendis assumenda, reiciendis soluta quisquam! Nesciunt ea laudantium quisquam maiores quia earum cupiditate quaerat molestiae?</p>
+        </div>
+    )
+}
